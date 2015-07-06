4103f0f796c2afcf59f534da7b19923317f16d21
Marc <mginov@gmail.com>
2015-07-06 18:53:31 +0200

    commit entidades

---
+++ src/AppBundle/Entity/Movie.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marc
+ * Date: 6/07/15
+ * Time: 18:46
+ */
+
+namespace AppBundle\Entity;
+
+
+class Movie
+{
+    protected $id;
+
+    protected $title;
+
+    protected $synopsis;
+}
---
+++ src/AppBundle/Entity/Person.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marc
+ * Date: 6/07/15
+ * Time: 18:26
+ */
+
+namespace AppBundle\Entity;
+
+use DateTime;
+
+class Person
+{
+    /**
+     * @var integer
+     *
+     * Id
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * Name
+     */
+    protected $name;
+
+    /**
+     * @var DateTime
+     *
+     * Created at
+     */
+    protected $createdAt;
+
+    /**
+     * @var DateTime
+     *
+     * Updated at
+     */
+    protected $updatedAt;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param DateTime $createdAt
+     * @return $this
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param DateTime $updatedAt
+     * @return $this
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
